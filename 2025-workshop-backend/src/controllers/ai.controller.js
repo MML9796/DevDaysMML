01b7e3552e715c72d78a5b9ec4f8ea087e3882c8
MML9796 <sansilama@alum.us.es>
2026-01-11 17:01:20 +0100

    nivel 1-3

--- 2025-workshop-backend/src/controllers/ai.controller.js
+++ 2025-workshop-backend/src/controllers/ai.controller.js
@@ -1,11 +1,9 @@
 // import { generateText } from '../services/openai.service.js';
- import { generateText } from '../services/ollama.service.js';
+ import { generateText } from '../services/ollama.service.js'; // hay que tener ollama corriendo localmente
 
 export const generateAIResponse = async (req, res) => {
     try {
         const { prompt } = req.body;
-        
-        // Validación básica
         if (!prompt) {
             return res.status(400).json({ message: "El prompt es obligatorio" });
         }
@@ -13,12 +11,11 @@ export const generateAIResponse = async (req, res) => {
         const aiResponse = await generateText(prompt);
         res.status(200).json({ response: aiResponse });
     } catch (error) {
-        // ¡IMPORTANTE! Imprime el error en la consola para verlo
         console.error("Error en OpenAI:", error); 
         
         res.status(500).json({ 
             message: 'Internal server error',
-            error: error.message // Opcional: enviar el error al frontend para debug
+            error: error.message 
         });
     }
 };
